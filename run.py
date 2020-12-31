1ef469d22eca89c188aad1a0ece87151288fcc2e
arif fadhillah <ariffadhillah319@gmail.com>
2020-12-31 12:53:18 -0500

    menggunakan luping untuk mengecek page/halaman

--- run.py
+++ run.py
@@ -10,47 +10,35 @@ def login():
     }
 
     res = session.post('http://127.0.0.1:5000/login', data=datas)
-    #
-    # f = open('./res.html', 'w+')
-    # f.write(res.text)
-    # f.close()
 
     soup = BeautifulSoup(res.text, 'html5lib')
 
     page_item = soup.find_all('li', attrs={'class': 'page-item'})
 
-    # print(len(page_item))
-
     total_pages = len(page_item) - 2
+
     return total_pages
 
 
-def get_urls():
+def get_urls(page):
     print('getting urls.....')
 
     params = {
-        'page': 1
+        'page': page
     }
-    # res = session.get('http://127.0.0.1:5000', params=params)
-    # soup = BeautifulSoup(res.text, 'html5lib')
-    soup = BeautifulSoup(open('./res.html'), 'html5lib')
+    res = session.get('http://127.0.0.1:5000', params=params)
+    soup = BeautifulSoup(res.text, 'html5lib')
+
 
 
     titles = soup.find_all('h4', attrs={'class': 'card-title'})
-
-    # menggunakna metode pengabungan url (urls = [], urls.append(url)
     urls = []
 
     for title in titles:
         url = title.find('a')['href']
         urls.append(url)
 
-        print(url)
-
-    # f = open('./res.html', 'w+')
-    # f.write(res.text)
-    # f.close()
-
+    return urls
 
 def get_detail():
     print('getting detail... ')
@@ -59,8 +47,11 @@ def create_csv():
     print('csv generated... ')
 
 def run() -> object:
-    # login()
-    get_urls()
+    total_pages = login()
+    for i in range(total_pages):
+        page = i + 1
+        print(page)
+    # get_urls()
     get_detail()
     create_csv()
 

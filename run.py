20aca217b648a75c732f6f23ef8e2a71de45c2f9
arif fadhillah <ariffadhillah319@gmail.com>
2020-12-31 12:39:48 -0500

    print url/ menggunakan metode open file di laptop

--- run.py
+++ run.py
@@ -10,25 +10,40 @@ def login():
     }
 
     res = session.post('http://127.0.0.1:5000/login', data=datas)
-
-    f = open('./res.html', 'w+')
-    f.write(res.text)
-    f.close()
+    #
+    # f = open('./res.html', 'w+')
+    # f.write(res.text)
+    # f.close()
 
     soup = BeautifulSoup(res.text, 'html5lib')
 
     page_item = soup.find_all('li', attrs={'class': 'page-item'})
 
-    print(len(page_item))
+    # print(len(page_item))
 
-
-
-
+    total_pages = len(page_item) - 2
+    return total_pages
 
 
 def get_urls():
     print('getting urls.....')
 
+    params = {
+        'page': 1
+    }
+    # res = session.get('http://127.0.0.1:5000', params=params)
+    # soup = BeautifulSoup(res.text, 'html5lib')
+    soup = BeautifulSoup(open('./res.html'), 'html5lib')
+
+
+    titles = soup.find_all('h4', attrs={'class': 'card-title'})
+    for title in titles:
+        url = title.find('a')['href']
+        print(url)
+
+    # f = open('./res.html', 'w+')
+    # f.write(res.text)
+    # f.close()
 
 
 def get_detail():
@@ -38,12 +53,11 @@ def create_csv():
     print('csv generated... ')
 
 def run() -> object:
-    login()
+    # login()
     get_urls()
     get_detail()
     create_csv()
 
 if __name__ == '__main__':
     run()
-
 

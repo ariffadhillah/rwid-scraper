6c39aa124d3adf6169f22d675203a2d058648f4b
arif fadhillah <ariffadhillah319@gmail.com>
2021-01-01 13:34:57 -0500

    done

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 import glob
 import json
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-
 
 session = requests.Session()
 
+
 def login():
-    print('login...')
+    print('login....')
     datas = {
         'username': 'user',
         'password': 'user12345'
@@ -19,38 +20,32 @@ def login():
     soup = BeautifulSoup(res.text, 'html5lib')
 
     page_item = soup.find_all('li', attrs={'class': 'page-item'})
-
     total_pages = len(page_item) - 2
 
     return total_pages
 
 
 def get_urls(page):
-    print('getting urls..... page {}', format(page))
+    print('getting urls..... page {}'.format(page))
     params = {
         'page': page
     }
     res = session.get('http://127.0.0.1:5000', params=params)
+
     soup = BeautifulSoup(res.text, 'html5lib')
-
-
 
     titles = soup.find_all('h4', attrs={'class': 'card-title'})
     urls = []
-
     for title in titles:
         url = title.find('a')['href']
         urls.append(url)
 
     return urls
 
+
 def get_detail(url):
     print('getting detail... {} '.format(url))
-    res = session.get('http://127.0.0.1:5000'+url)
-
-    # f = open('./res.html ', 'w+')
-    # f.write(res.text)
-    # f.close()
+    res = session.get('http://127.0.0.1:5000' + url)
 
     soup = BeautifulSoup(res.text, 'html5lib')
     title = soup.find('title').text.strip()
@@ -70,8 +65,10 @@ def get_detail(url):
     with open('./results/{}.json'.format(url.replace('/', '')), 'w') as outfile:
         json.dump(dict_data, outfile)
 
+
 def create_csv():
     files = sorted(glob.glob('./results/*.json'))
+
     datas = []
     for file in files:
         with open(file) as json_file:
@@ -81,34 +78,34 @@ def create_csv():
     df = pd.DataFrame(datas)
     df.to_csv('results.csv', index=False)
 
-    print('csv generated... ')
-
+    print('csv generated....')
 
 
 def run():
     total_pages = login()
 
-    total_urls = []
-    for i in range(total_pages):
-        page = i + 1
-        urls = get_urls(page)
-        total_urls += urls  # total urls = total url + urls
+    options = int(input('Input option number:\n1.collecting all urls\n2.get detail all products\n3.create csv:\n '))
+    if options == 1:
+        total_urls = []
+        for i in range(total_pages):
+            page = i + 1
+            urls = get_urls(page)
+            total_urls += urls  # total_urls = total_urls + urls
+        with open('all_urls.json', 'w') as outfile:
+            json.dump(total_urls, outfile)
 
-    with open('all_urls.json', 'w') as outfile:
-        json.dump(total_urls, outfile)
+    if options == 2:
+        with open('all_urls.json') as json_file:
+            all_url = json.load(json_file)
 
-    # untuk mendapatkan semua result/(url) dan menyimpannya kedalam format json
-    with open('all_urls.json') as json_file:
-        all_url = json.load(json_file)
+        for url in all_url:
+            get_detail(url)
 
-    for url in all_url:
-        get_detail(url)
-    # end
+    if options == 3:
+        create_csv()
 
-    create_csv()
 
 if __name__ == '__main__':
     run()
-
 
 

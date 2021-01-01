b378efb8a855d70d12ca969a3e8028955e2c79f2
arif fadhillah <ariffadhillah319@gmail.com>
2021-01-01 12:29:44 -0500

    menyimpan urls/title kedalam format json

--- run.py
+++ run.py
@@ -40,24 +40,52 @@ def get_urls(page):
 
     return urls
 
-def get_detail():
+def get_detail(url):
     print('getting detail... ')
+    res = session.get('http://127.0.0.1:5000'+url)
+    f = open('./res.html ', 'w+')
+    f.write(res.text)
+    f.close()
+
+    soup = BeautifulSoup(res.text, 'html5lib')
+    title = soup.find('title').text.strip()
+    price = soup.find('h4', attrs={'class': 'card-price'}).text.strip()
+    stock = soup.find('span', attrs={'class': 'card-stock'}).text.strip().replace('stock: ', '')
+    category = soup.find('span', attrs={'class': 'card-category'}).text.strip().replace('category: ', '')
+    description = soup.find('p', attrs={'class': 'card-text'}).text.strip().replace('Description: ', '')
+
+    dict_data = {
+        'title': title,
+        'price': price,
+        'stock': stock,
+        'category': category,
+        'description': description
+    }
+
+    with open('./results/{}.json'.format(url.replace('/', '')), 'w') as outfile:
+        json.dump(dict_data, outfile)
 
 def create_csv():
     print('csv generated... ')
 
 def run():
     total_pages = login()
-    total_urls = []
-    for i in range(total_pages):
-        page = i + 1
-        urls = get_urls(page)
-        total_urls += urls  # total urls = total url + urls
 
-    with open('all_urls.json', 'w') as outfile:
-        json.dump(total_urls, outfile)
+    # total_urls = []
+    # for i in range(total_pages):
+    #     page = i + 1
+    #     urls = get_urls(page)
+    #     total_urls += urls  # total urls = total url + urls
+    #
+    # with open('all_urls.json', 'w') as outfile:
+    #     json.dump(total_urls, outfile)
 
-    get_detail()
+    with open('all_urls.json') as json_file:
+        all_url = json.load(json_file)
+        get_detail('/takoyakids-everyday-darling-rabbit-sets-girl-yellow')
+    # for url in all_url:
+    #     get_detail(url)
+
     create_csv()
 
 if __name__ == '__main__':
